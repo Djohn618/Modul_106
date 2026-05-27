89b789704900d0207d380ccf368a50ab045bdc54
Djohn618 <deshan618@icloud.com>
2026-05-27 12:47:37 +0200

    first commit

--- SQL/JOIN_Abfragen.sql
+++ SQL/JOIN_Abfragen.sql
@@ -1,4 +1,8 @@
 Show databases;
-use flugschule;
-drop database flugschule;
+Use sakila;
 
+-- 1
+SELECT f.title, f.description, c.name AS Kategorie
+FROM film f 
+LEFT JOIN category c ON f.film_id = c.film_id
+WHERE f.release_year = 1999;
